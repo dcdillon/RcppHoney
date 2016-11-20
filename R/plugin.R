6531f2d5ace6f1d7884342fc8b3d4516c6616f65
Daniel C. Dillon <dcdillon@gmail.com>
2016-11-20 16:45:00 -0600

    Seriously...fixed this time.

--- R/plugin.R
+++ R/plugin.R
@@ -5,7 +5,7 @@
     shlib_cxxld <- system("R CMD config SHLIB_CXXLD", intern=TRUE)
 
     shlib_ld_results <- system(paste0(shlib_ld, " --version"), intern=TRUE)
-    shlib_cxxld <- system(paste0(shlib_cxxld, " --version"), intern=TRUE)
+    shlib_cxxld_results <- system(paste0(shlib_cxxld, " --version"), intern=TRUE)
 
     shlib_ld_good <- any(grepl(linker_pattern, shlib_ld_results))
     shlib_cxxld_good <- any(grepl(linker_pattern, shlib_cxxld_results))
