f51899f787658b9aae78698305682ce48d9e0e4f
Daniel C. Dillon <dcdillon@gmail.com>
2016-12-01 18:16:08 -0600

    Added copyright notice to plugin.R

--- R/plugin.R
+++ R/plugin.R
@@ -1,3 +1,20 @@
+# Copyright (C) 2016 Daniel C. Dillon
+#
+# This file is part of RcppHoney.
+#
+# RcppHoney is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# RcppHoney is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RcppHoney.  If not, see <http://www.gnu.org/licenses/>.
+
 # Return the appropriate option to pass the linker to cause it to strip
 # debugging symbols out of a shared library.  This only takes actual linkers
 # (ld, gold, etc.) and not "wrappers" (gcc, clang, etc.).  Returns the option
