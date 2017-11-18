b735b8670ab59067269a5ef158c5e2a7718bae44
Daniel C. Dillon <dcdillon@gmail.com>
2017-11-18 17:18:46 -0600

    Ading some stuff for CRAN, I guess...

---
+++ src/annoyance.c
@@ -0,0 +1,9 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_RcppHoney(DllInfo* info)
+{
+    R_registerRoutines(info, NULL, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, TRUE);
+}
